2bec40b519931361a8db32886ed80320a06af1cc
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-02-14 20:04:58 +0100

    style: switch theme to dark

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header elevated class="bg-dark">
       <q-toolbar>
         <q-toolbar-title>Digital Logic Sim</q-toolbar-title>
       </q-toolbar>
