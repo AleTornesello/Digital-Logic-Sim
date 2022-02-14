ebd4429ad2603054dfdb3c89b286e594c2c97ca1
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-02-14 23:32:37 +0100

    feat: add drawer for existing nodes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,31 +1,58 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-dark">
+  <q-layout view="hHh LpR fFf">
+    <q-header class="bg-dark" bordered>
       <q-toolbar>
         <q-toolbar-title>Digital Logic Sim</q-toolbar-title>
       </q-toolbar>
 
       <q-tabs v-model="openedTab" dense class="bg-dark text-white">
         <q-tab
-          v-for="openNode in openNodes"
-          :key="openNode.id"
-          :name="openNode.id"
+          v-for="openNodeId in openNodes"
+          :key="openNodeId"
+          :name="openNodeId"
         >
           <span>
-            {{ openNode.name }}
+            {{ getNodeById(openNodeId).name }}
             <q-btn flat padding="none">
-              <q-icon name="close" @click="removeNode()"></q-icon>
+              <q-icon name="close" @click="removeNode(openNodeId)"></q-icon>
             </q-btn>
           </span>
         </q-tab>
         <q-btn flat>
-          <q-icon name="add" @click="addNode()"></q-icon>
+          <q-icon name="add" @click="addNewNode()"></q-icon>
         </q-btn>
       </q-tabs>
-      <q-separator />
     </q-header>
 
+    <q-drawer
+      v-model="nodesDrawerModel"
+      show-if-above
+      :breakpoint="700"
+      class="bg-primary text-white"
+    >
+      <q-scroll-area class="fit">
+        <div class="q-pa-sm">
+          <div
+            v-for="node in nodes"
+            :key="node.id"
+            class="node_preview"
+            :style="{ 'background-color': node.color }"
+          >
+            {{ node.name }}
+          </div>
+        </div>
+      </q-scroll-area>
+    </q-drawer>
+
     <q-page-container>
+      <q-btn
+        unelevated
+        color="primary"
+        @click="toggleNodesDrawer()"
+        id="nodes-drawer-toggle"
+      >
+        <q-icon name="menu"></q-icon>
+      </q-btn>
       <router-view />
     </q-page-container>
   </q-layout>
@@ -34,38 +61,55 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { Node } from 'src/models/NodeModel';
+import { uid } from 'quasar';
 
 export default defineComponent({
   name: 'MainLayout',
   data() {
     return {
       openedTab: '0',
-      openNodes: Array.of<Node>(
+      openNodes: ['0'],
+      nodes: Array.of<Node>(
         new Node({
           id: '0',
           name: 'New Node',
         })
       ),
+      nodesDrawerModel: true,
     };
   },
   methods: {
-    addNode(): void {
-      this.openNodes.push(
-        new Node({
-          id: this.openNodes.length.toString(),
-          name: 'New Node',
-        })
-      );
+    addNewNode(): void {
+      const id: string = uid();
+      this.nodes.push(new Node({ id, name: 'New Node' }));
+      this.openNodes.push(id);
     },
-    removeNode(): void {
-      const nodeIndex = this.openNodes.findIndex(
-        (node) => node.id === this.openedTab
-      );
+    removeNode(id: string): void {
+      const nodeIndex = this.openNodes.findIndex((nodeId) => nodeId === id);
 
       if (nodeIndex !== -1) {
         this.openNodes.splice(nodeIndex, 1);
       }
     },
+    getNodeById(id: string): Node | undefined {
+      return this.nodes.find((node) => node.id === id);
+    },
+    toggleNodesDrawer(): void {
+      this.nodesDrawerModel = !this.nodesDrawerModel;
+    },
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.node_preview {
+  border: 1px solid #cccccc;
+  border-radius: 4px;
+  padding: 12px 8px;
+  color: #000000;
+}
+
+#nodes-drawer-toggle {
+  border-radius: 0 4px 4px 0;
+}
+</style>
