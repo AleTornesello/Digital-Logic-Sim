acf70f081d2d5f7cc78c7e97d78fed9fb7e8cf1c
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-02-16 23:58:12 +0100

    feat: create events bus

---
+++ src/boot/global-event-bus.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { TinyEmitter } from 'tiny-emitter';
+const emitter = new TinyEmitter();
+
+export default ({ app }) => {
+  app.config.globalProperties.$global = {
+    $on: (...args) => emitter.on(...args),
+    $once: (...args) => emitter.once(...args),
+    $off: (...args) => emitter.off(...args),
+    $emit: (...args) => emitter.emit(...args),
+  };
+};
+
+export { emitter };
