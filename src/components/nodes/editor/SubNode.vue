ca9245cf657e8b99ae99d79437b862fce2d1b609
AleTornesello99 <alessandro.tornesello99@gmail.com>
2022-02-17 23:08:48 +0100

    feat: create component for sub node (still empty) in node editor

---
+++ src/components/nodes/editor/SubNode.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>aaaa</div>
+</template>
+
+<script lang="ts">
+import { Node } from 'src/models/NodeModel';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SubNode',
+  props: {
+    node: {
+      type: Node,
+      required: true,
+    },
+  },
+});
+</script>
